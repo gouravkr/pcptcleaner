299134183d37e0a94ddcb15e276d5a13ae3a5fcd
Gourav <gourav2711@gmail.com>
2024-05-20 12:11:13 +0530

    individual imports to reduce bundle size

--- src/main.js
+++ src/main.js
@@ -4,12 +4,20 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-import ElementPlus from 'element-plus'
+// import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+
+import { ElButton, ElRow, ElCol, ElSwitch, ElInput }  from 'element-plus'
+
 
 const app = createApp(App)
 
 app.use(router)
-app.use(ElementPlus)
+// app.use(ElementPlus)
+app.component('ElButton', ElButton)
+app.component('ElRow', ElRow)
+app.component('ElCol', ElCol)
+app.component('ElSwitch', ElSwitch)
+app.component('ElInput', ElInput)
 
 app.mount('#app')
